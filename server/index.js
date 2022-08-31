06b4b89eb0c52c2c6f27d26cffcb7bb4b6d125e7
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-31 08:16:55 -0500

    server configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import morgan from 'morgan'
+import { Server as SockerServer } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import { PORT } from './config.js' //si vamos a tarer nuestros propios modulos tienes que llear la extension .js
 
 const app = express()
+const server = http.createServer(app)
+const io = new SockerServer(server)
 
-app.listen(3000)
-console.log('server on port 3000')
+app.use(cors)
+app.use(morgan('dev'))
+
+app.listen(PORT)
+console.log('server on port ' + PORT)
