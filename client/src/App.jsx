7879517bfab6d14b35c3a31c044bd6251bcd7187
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-31 17:49:40 -0500

    interfaces for messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,22 @@ const socket = io("http://localhost:4000");
 function App() {
   const [message, setMessage] = useState("");
 
+  const [messages, setMessages] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     socket.emit("message", message);
+    const newMessage = {
+      body: message,
+      from: "Me"
+    }
+    setMessages([newMessage,...messages])
     setMessage("");
   };
 
   useEffect(() => {
     const recivedMenssage = (message) => {
+      setMessages([message, ...messages]);
       console.log(message);
     };
     socket.on("message", recivedMenssage);
@@ -22,14 +30,20 @@ function App() {
     return () => {
       socket.off("message", recivedMenssage);
     };
-  }, []);
+  }, [messages]);
 
   return (
     <div>
-      <form onSubmit={handleSubmit} value={message}>
-        <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} />
         <button type="submit">Send</button>
       </form>
+
+      {messages.map((message, index) => (
+        <div key={index}>
+          <p>{message.from}: {message.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
-import express from 'express'
-import morgan from 'morgan'
-import { Server as SockerServer } from 'socket.io'
-import http from 'http'
-import cors from 'cors'
-import { PORT } from './config.js' //si vamos a tarer nuestros propios modulos tienes que llear la extension .js
+import express from "express"
+import morgan from "morgan"
+import { Server as SockerServer } from "socket.io"
+import http from "http"
+import cors from "cors"
+import { PORT } from "./config.js" //si vamos a tarer nuestros propios modulos tienes que llear la extension .js
 
 const app = express()
 const server = http.createServer(app)
 const io = new SockerServer(server, {
     cors: {
-        origin: '*' // para que se conecte cualquier servidor
+        origin: "*" // para que se conecte cualquier servidor
     }
 })
 
 app.use(cors)
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     console.log(socket.id)
 
-    socket.on('message', (message) => {
+    socket.on("message", (message) => {
         console.log(message)
-        socket.broadcast.emit('message', message)
+        socket.broadcast.emit("message", {
+            body: message,
+            from: socket.id
+        })
     })
 })
 
 server.listen(PORT)
-console.log('server on port ' + PORT)
+console.log("server on port " + PORT)
